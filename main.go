9f2ab7d0068181eee0ed8de91e1e930d76299620
Caitlin Elfring <caitlin@graciouseloise.com>
2018-05-08 09:00:06 -0400

    Basic html template to display gif and colors

--- main.go
+++ main.go
@@ -1,36 +1,83 @@
 package main
 
 import (
+	"html/template"
+	"image/color"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/caitlin615/gif-battle/colors"
 	"github.com/caitlin615/gif-battle/giphy"
 	"github.com/caitlin615/gif-battle/kmeans"
 )
 
-func main() {
-	// giphyGIF, err := giphy.Random("burrito")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gifURL := giphyGIF.Images.DownsizedStill.URL
-	gifURL := "https://media2.giphy.com/media/jtKpyFWJrVI08/giphy.gif"
+var templates = template.Must(template.ParseGlob("templates/*.html"))
 
-	gifObject, err := giphy.GetGIF(gifURL)
+type templateData struct {
+	Left  battleImage
+	Right battleImage
+}
+
+type battleImage struct {
+	Image         *giphy.Image
+	Colors        []color.Color
+	MostUsedColor color.Color
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// "/" catches all paths, but we only want to execute the template on the root
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		data := templateData{
+			Left:  newBattleGif(),
+			Right: newBattleGif(),
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	port := "8080"
+	if p, ok := os.LookupEnv("PORT"); ok && port != "" {
+		port = p
+	}
+	addr := "0.0.0.0:" + port
+	log.Println("Listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func newBattleGif() (b battleImage) {
+	giphyGIF, err := giphy.Random("burrito")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
+	}
+	b.Image = &giphyGIF.Images.Original
+	gifObject, err := giphy.GetGIF(giphyGIF.Images.DownsizedStill.URL)
+	if err != nil {
+		log.Println(err)
+		return
 	}
 	frame := gifObject.Image[0]
 
-	// using most common color
-	mostUsedColor := colors.GetMostUsedColor(frame)
-	log.Println("Most used color:", colors.ColorToHex(mostUsedColor))
-
-	// using k-means clustering
 	clrs := colors.GetPixels(frame)
-	km := kmeans.NewKmeansClustering(2, 10, 10)
+
+	mostUsedColor := colors.GetMostUsedColor(frame)
+	b.MostUsedColor = mostUsedColor
+
+	km := kmeans.NewKmeansClustering(2, 3, 10)
 	prominantColors := km.Run(clrs)
+
 	for _, color := range prominantColors {
-		log.Println(colors.ColorToHex(color))
+		b.Colors = append(b.Colors, color)
 	}
+	return
 }
